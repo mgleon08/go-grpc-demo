549979799b00ac0b2c5d269bb92a190aa9c56a66
Leon Ji <mgleon08@gmail.com>
2019-02-28 21:46:16 +0800

    client

---
+++ client/main.go
@@ -0,0 +1,34 @@
+package main
+
+import (
+	"context"
+	"log"
+
+	"google.golang.org/grpc"
+
+	"github.com/mgleon08/demo_proto/pb"
+)
+
+const (
+	address = "localhost:50051"
+)
+
+func main() {
+	// 建立連線到遠端 gRPC 伺服器。
+	conn, err := grpc.Dial(address, grpc.WithInsecure())
+	if err != nil {
+		log.Fatalf("無法連線: %v", err)
+	}
+	// 結束後要關閉
+	defer conn.Close()
+
+	// 建立新的 Calculator 客戶端，所以等一下就能夠使用 Calculator 的所有方法。
+	c := pb.NewCalculatorClient(conn)
+
+	// 傳送新請求到遠端 gRPC 伺服器 Calculator 中，並呼叫 Plus 函式，讓兩個數字相加。
+	r, err := c.Plus(context.Background(), &pb.CalcRequest{NumberA: 32, NumberB: 32})
+	if err != nil {
+		log.Fatalf("無法執行 plus: %v", err)
+	}
+	log.Printf("回傳結果: %d", r.Result)
+}
